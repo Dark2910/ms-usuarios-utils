03e1c07575c81dbeafc05d890a6268240d6a1e88
Trazo <Espindola.A.E@hotmail.com>
2026-08-23 14:08:53 -0600

    REFACTOR-builder

--- src/main/java/com/eespindola/ms_usuarios_utils/model/Result.java
+++ src/main/java/com/eespindola/ms_usuarios_utils/model/Result.java
@@ -2,16 +2,11 @@ package com.eespindola.ms_usuarios_utils.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result<T> {
 
@@ -33,21 +28,88 @@ public class Result<T> {
   @JsonProperty("errorDescription")
   private List<String> errorDescription;
 
-    // Patron Builder
-    public Result(ResultBuilder<T> builder) {
-        this.success = builder.success;
-        this.message = builder.message;
-        this.data = builder.data;
-        this.dataList = builder.dataList;
-        this.errorCode = builder.errorCode;
-        this.errorDescription = builder.errorDescription;
-    }
+  // ==========================================
+  // 2. CONSTRUCTORES
+  // ==========================================
 
-  public static <T> ResultBuilder<T> builder(){
+  public Result() {
+  }
+
+  private Result(ResultBuilder<T> builder) {
+    this.success = builder.success;
+    this.message = builder.message;
+    this.data = builder.data;
+    this.dataList = builder.dataList;
+    this.errorCode = builder.errorCode;
+    this.errorDescription = builder.errorDescription;
+  }
+
+  // ==========================================
+  // 3. MÉTODOS ESTÁTICOS
+  // ==========================================
+
+  public static <T> ResultBuilder<T> builder() {
     return new ResultBuilder<>();
   }
 
-  public static class ResultBuilder<T>{
+  // ==========================================
+  // 4. GETTERS Y SETTERS
+  // ==========================================
+
+  public Boolean getSuccess() {
+    return success;
+  }
+
+  public void setSuccess(Boolean success) {
+    this.success = success;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public List<T> getDataList() {
+    return dataList;
+  }
+
+  public void setDataList(List<T> dataList) {
+    this.dataList = dataList;
+  }
+
+  public Integer getErrorCode() {
+    return errorCode;
+  }
+
+  public void setErrorCode(Integer errorCode) {
+    this.errorCode = errorCode;
+  }
+
+  public List<String> getErrorDescription() {
+    return errorDescription;
+  }
+
+  public void setErrorDescription(List<String> errorDescription) {
+    this.errorDescription = errorDescription;
+  }
+
+  // ==========================================
+  // 5. CLASE BUILDER
+  // ==========================================
+
+  public static class ResultBuilder<T> {
+
     private Boolean success;
     private String message;
     private T data;
@@ -55,23 +117,22 @@ public class Result<T> {
     private Integer errorCode;
     private List<String> errorDescription;
 
-
     public ResultBuilder<T> success(Boolean success) {
-        this.success = success;
+      this.success = success;
       return this;
     }
 
-    public ResultBuilder<T> message(String message){
+    public ResultBuilder<T> message(String message) {
       this.message = message;
       return this;
     }
 
-    public ResultBuilder<T> data(T data){
+    public ResultBuilder<T> data(T data) {
       this.data = data;
       return this;
     }
 
-    public ResultBuilder<T> dataList(List<T> dataList){
+    public ResultBuilder<T> dataList(List<T> dataList) {
       this.dataList = dataList;
       return this;
     }
@@ -81,15 +142,14 @@ public class Result<T> {
       return this;
     }
 
-    public ResultBuilder<T> errorDescription(List<String> errorDescription){
-        this.errorDescription = errorDescription;
-        return this;
+    public ResultBuilder<T> errorDescription(List<String> errorDescription) {
+      this.errorDescription = errorDescription;
+      return this;
     }
 
-    public Result<T> build(){
-        return new Result<>(this);
+    public Result<T> build() {
+      return new Result<>(this);
     }
 
   }
-
 }
